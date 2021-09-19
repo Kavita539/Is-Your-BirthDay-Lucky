29d7ec50f2aaea5192168c8a1a031d9546910e5d
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2021-09-19 15:39:28 +0530

    docs : Updated code

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const dateOfBirth = document.querySelector("#date-of-birth");;
-const luckyNumber = document.querySelector("#lucky-num");
+const luckyNum = document.querySelector("#lucky-num");
 const checkNumberButton = document.querySelector("#check-num");
 const outputBox = document.querySelector("#output-box");
-var crossIcon= document.querySelector(".cross");
-var noteDiv= document.querySelector(".note");
-var luckyDiv= document.querySelector(".lucky");
-var unluckyDiv= document.querySelector(".unlucky");
+var crossIcon = document.querySelector(".cross");
+var noteDiv = document.querySelector(".note");
+var form = document.querySelector("#form");
+var luckyDiv = document.querySelector(".lucky");
+var unluckyDiv = document.querySelector(".unlucky");
 
-
-function compareValues(sum,luckyNumber){
-    if(sum%luckyNumber===0){
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    var dob = dateOfBirth.value;
+    dob = dob.replaceAll("-","");
+     let sum = 0;
+     for(let index=0; index < dob.length; index++){
+         sum = sum + Number(dob.charAt(index));
+     }
+     const luckyNumber = Number(luckyNum.value);
+     if(sum % luckyNumber === 0){
         outputBox.innerText="Yay! Your birthday is lucky";
         luckyDiv.style.display="block";
         unluckyDiv.style.display="none";
@@ -18,29 +26,9 @@ function compareValues(sum,luckyNumber){
         unluckyDiv.style.display="block";
         luckyDiv.style.display="none";
     }
-}
-
-function checkBirthdayIsLucky(){
-    const dob = dateOfBirth.value;
-    const sum = calculateSum(dob);
-    if(sum&&dob)
-    compareValues(sum,luckyNumber.value);
-    else
-    outputBox.innerText="Please enter both the fields";  
-}
-
-function calculateSum(dob){
-     dob = dob.replaceAll("-","");
-     let sum = 0;
-     for(let index=0; index < dob.length; index++){
-         sum = sum + Number(dob.charAt(index));
-     }
-     return sum;
-}
-
-checkNumberButton.addEventListener('click', checkBirthdayIsLucky);
+})
 
 crossIcon.addEventListener("click", ()=>{
     noteDiv.style.display="none";
-    console.log("working");
-});
+    // console.log("working");
+})
